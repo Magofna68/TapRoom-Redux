8c616de5aa353d19bdc9a81c83a6beb88349f802
Brandon-Magofna <magofna68@gmail.com>
2021-07-02 09:15:13 -0700

    :hammer: built out kegList.js

---
+++ taproom/src/Components/KegList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Keg from './Keg';
+import PropTypes from 'prop-types';
+
+function KegList(props) {
+  return (
+    <React.Fragment>
+      <hr />
+      {props.kegList.map((ticket) =>
+        <Keg
+          whenKegClicked={props.onKegSelection}
+          name={keg.name}
+          brand={keg.brand}
+          content={keg.content}
+          price={keg.price}
+          id={keg.id}
+          key={keg.id} />
+      )}
+    </React.Fragment>
+  );
+}
+
+KegList.propTypes = {
+  kegList: PropTypes.array,
+  onKegSelection: PropTypes.func
+};
+
+export default KegList;
