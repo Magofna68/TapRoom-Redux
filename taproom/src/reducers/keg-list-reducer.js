cb9338bd26fb60fea0702e70f3417f5d39baec9a
Brandon-Magofna <magofna68@gmail.com>
2021-07-09 09:36:37 -0700

    attempt to squash another :bug:

--- taproom/src/reducers/keg-list-reducer.js
+++ taproom/src/reducers/keg-list-reducer.js
@@ -3,7 +3,7 @@ import * as c from './../actions/actionTypes';
 export default (state = {}, action) => {
   const { name, brand, price, quantity, content, id, type } = action;
   switch (type) {
-    case c.ADD_KEG:
+    case 'ADD_KEG':
       return Object.assign({}, state, {
         [id]: {
           name: name,
