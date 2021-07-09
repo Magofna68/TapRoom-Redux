c30a7afc1522748c5ea7527e841dd45de247a360
Brandon-Magofna <magofna68@gmail.com>
2021-07-09 11:24:41 -0700

    9th & 10th test: passing :white_check_mark:

--- taproom/src/__tests__/reducers/index-reducer.test.js
+++ taproom/src/__tests__/reducers/index-reducer.test.js
@@ -2,6 +2,8 @@ import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
 import formVisibleReducer from '../../reducers/form-visible-reducer';
 import kegListReducer from '../../reducers/keg-list-reducer';
+import { TOGGLE_FORM } from '../../actions/actionTypes';
+import * as c from '../../actions/actionTypes';
 
 let store = createStore(rootReducer);
 
@@ -20,6 +22,28 @@ describe('rootReducer', () => {
 
   test('Check that initial state of formVisibleReducer matches root reducer', () => {
     expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, { type: null }));
+  });
+
+  test('Check that ADD_KEG action works for kegListReducer and root reducer', () => {
+    const action = {
+      type: c.ADD_KEG,
+      name: 'name',
+      brand: 'brand',
+      content: 'content',
+      price: 'price',
+      quantity: 'quantity',
+      id: 'id'
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that TOGGLE_FORM action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: c.TOGGLE_FORM
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action));
   });
 
 });
